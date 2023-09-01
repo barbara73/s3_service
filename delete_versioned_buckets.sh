ffeca09e0f7c13619fb1c7b5732ca304ad6b253e
Barbara <barbara.jesacher@gmail.com>
2023-09-01 09:03:54 +0200

    Update delete_versioned_buckets.sh

--- delete_versioned_buckets.sh
+++ delete_versioned_buckets.sh
@@ -6,8 +6,8 @@ set -e
 
 echo "Removing all versions from $bucket"
 
-versions=`aws s3api --endpoint-url https://s3-test.usz.ch list-object-versions --bucket $bucket --ca-bundle /etc/pki/ca-trust/source/anchors/USZ-Root-CA-2.pem |jq '.Versions'`
-markers=`aws s3api --endpoint-url https://s3-test.usz.ch list-object-versions --bucket $bucket --ca-bundle /etc/pki/ca-trust/source/anchors/USZ-Root-CA-2.pem |jq '.DeleteMarkers'` 
+versions=`aws s3api --endpoint-url https://s3url.ch list-object-versions --bucket $bucket --ca-bundle CA.pem |jq '.Versions'`
+markers=`aws s3api --endpoint-url https://s3url.ch list-object-versions --bucket $bucket --ca-bundle CA.pem |jq '.DeleteMarkers'` 
 
 echo "removing files"
 for version in $(echo "${versions}" | jq -r '.[] | @base64'); do 
@@ -15,7 +15,7 @@ for version in $(echo "${versions}" | jq -r '.[] | @base64'); do
 
     key=`echo $version | jq -r .Key`
     versionId=`echo $version | jq -r .VersionId `
-    cmd="aws s3api --endpoint-url https://s3-test.usz.ch delete-object --bucket $bucket --key $key --version-id $versionId --ca-bundle /etc/pki/ca-trust/source/anchors/USZ-Root-CA-2.pem"
+    cmd="aws s3api --endpoint-url https://s3url.ch delete-object --bucket $bucket --key $key --version-id $versionId --ca-bundle CA-2.pem"
     echo $cmd
     $cmd
 done
@@ -26,7 +26,7 @@ for marker in $(echo "${markers}" | jq -r '.[] | @base64'); do
 
     key=`echo $marker | jq -r .Key`
     versionId=`echo $marker | jq -r .VersionId `
-    cmd="aws s3api --endpoint-url https://s3-test.usz.ch delete-object --bucket $bucket --key $key --version-id $versionId --ca-bundle /etc/pki/ca-trust/source/anchors/USZ-Root-CA-2.pem"
+    cmd="aws s3api --endpoint-url https://s3url.ch delete-object --bucket $bucket --key $key --version-id $versionId --ca-bundle CA-2.pem"
     echo $cmd
     $cmd
 done
